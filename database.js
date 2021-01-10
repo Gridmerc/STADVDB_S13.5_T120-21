15fbb01eb9e31a6d209ad98dbf4ba31596fe6cdc
antonstaana <54714576+antonstaana@users.noreply.github.com>
2021-01-11 06:10:53 +0800

    initial commit

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-
+/*
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -7,4 +7,14 @@ const db = mysql.createConnection({
     port: '3306',
     database: 'creditcards'
 });
+*/
+
+const db = mysql.createConnection({
+    host: 'us-cdbr-east-02.cleardb.com',
+    user: 'b544cbd4569ff0',
+    password: 'p@f05669d5',
+    port: '3306',
+    database: 'heroku_25b3344a3871b1e'
+});
+
 module.exports = db;

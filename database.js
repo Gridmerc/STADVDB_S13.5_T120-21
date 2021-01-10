05c3765b50438c1afe511cd93b1f390b4f612aa7
antonstaana <54714576+antonstaana@users.noreply.github.com>
2021-01-11 06:21:32 +0800

    heroku patch 2

--- database.js
+++ database.js
@@ -12,7 +12,7 @@ const db = mysql.createConnection({
 const db = mysql.createConnection({
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'b544cbd4569ff0',
-    password: 'p@f05669d5',
+    password: 'f05669d5',
     database: 'heroku_25b3344a3871b1e'
 });
 

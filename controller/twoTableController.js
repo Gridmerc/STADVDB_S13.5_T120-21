ec286d115f3c4b8784ecf34c608680cf3e74962a
Gridmerc <58649232+Gridmerc@users.noreply.github.com>
2021-01-11 11:16:06 +0800

    Controller for Table Two
    Self-explanatory title.

---
+++ controller/twoTableController.js
@@ -0,0 +1,54 @@
+const db = require('../database');
+
+const oneTableController = {
+    getTwoTableOne: function(req, res) {
+        let sql = 'SELECT client_id, client_name ' +
+                  'FROM CLIENTS C JOIN CARDS CA ' +
+                  'ON C.card_category_id = CA.card_id ' +
+                  'WHERE CA.card_type = "Platinum";';
+        db.query(sql, function(err, data) {
+            if(err) throw err;
+            res.render('oneTableOne.ejs', {title: 'Two Table - First Query', userData: data});
+        });
+    },
+
+    getTwoTableTwo: function(req, res) {
+        let sql = 'SELECT COUNT(C.clientID) AS "Count", E.education_type AS "Eductype" ' +
+                  'FROM CLIENTS C JOIN EDUCATION_LEVELS E ON C.education_level_id = E.education_id ' +
+                  'WHERE age < 30 ' +
+                  'GROUP BY E.education_type ' +
+                  'SORT BY E.education_type;';
+        db.query(sql, function(err, data) {
+            if(err) throw err;
+            res.render('oneTableTwo.ejs', {title: 'One Table - Second Query', userData: data});
+        });
+    },
+
+    postTwoTableOne: function(req, res) {
+        let card = req.body.card;
+        let sql = 'SELECT C.client_id, C.client_name ' +
+                  'FROM CLIENTS C JOIN CARDS CA ' +
+                  'ON C.card_category_id = CA.card_id ' +
+                  'WHERE CA.card_type = ' + card + ';';
+        db.query(sql, function(err, data) {
+            if(err) throw err;
+            res.render('oneTableOne.ejs', {title: 'Two Table - First Query', userData: data});
+        });
+    },
+
+    postTwoTableTwo: function(req, res) {
+        let age = req.body.age;
+        let comparison = req.body.comparison;
+        let sql = "SELECT COUNT(C.clientID) AS 'Count', E.education_type AS 'Eductype' " +
+                  'FROM CLIENTS C JOIN EDUCATION_LEVELS E ON C.education_level_id = E.education_id ' +
+                  'WHERE age ' + comparison + ' ' +  age + ' ' +
+                  'GROUP BY E.education_type ' +
+                  'SORT BY E.education_type;';
+        db.query(sql, function(err, data) {
+            if(err) throw err;
+            res.render('oneTableTwo.ejs', {title: 'One Table - Second Query', userData: data});
+        });
+    }
+}
+
+module.exports = oneTableController;
--- routes/PublicRoutes.js
+++ routes/PublicRoutes.js
@@ -4,6 +4,7 @@ module.exports = router;
 const PublicController = require('../controller/PublicController');
 
 const oneTableController = require('../controller/oneTableController');
+const twoTableController = require('../controller/twoTableController');
 
 router.get('/', PublicController.HomePage);
 router.get('/twoTablesOne', PublicController.twoTablesOne);
@@ -14,6 +15,10 @@ router.get('/fourTables', PublicController.fourTables);
 
 router.get('/oneTableOne', oneTableController.getOneTableOne);
 router.get('/oneTableTwo', oneTableController.getOneTableTwo);
+router.get('/twoTablesOne', twoTableController.getTwoTableOne);
+router.get('/twoTablesTwo', twoTableController.getTwoTableTwo);
 
 router.post('/oneTableOne', oneTableController.postOneTableOne);
 router.post('/oneTableTwo', oneTableController.postOneTableTwo);
+router.post('/twoTablesOne', twoTableController.postTwoTableOne);
+router.post('/twoTablesTwo', twoTableController.postTwoTableTwo);

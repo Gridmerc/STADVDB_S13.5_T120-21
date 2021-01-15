b02653e32d5f66c9948fb74856e82a2810b89169
James Kevin Lin <lin.james0825@yahoo.com>
2021-01-15 19:35:20 +0800

    upload put index scripts

---
+++ Query Testing/optimized_db_queries/optimized_db_add_index_involved_columns_only.sql
@@ -0,0 +1,15 @@
+
+Create index idx_cards_card_type on cards(card_type);
+
+Create index idx_credit_clients_age on credit_clients(age);
+
+Create index idx_credit_clients_dependent_count on credit_clients(dependent_count);
+
+
+Create index idx_education_levels_education_type on education_levels(education_type);
+
+Create index idx_incomes_income_type on incomes(income_type);
+
+Create index idx_marital_statuses_marital_type on marital_statuses(marital_type);
+
+
---
+++ Query Testing/optimized_db_queries/optimized_db_add_index_primary_keys_only.sql
@@ -0,0 +1,13 @@
+Create index idx_cards_card_id on cards(card_id);
+-- Create index idx_cards_card_type on cards(card_type);
+Create index idx_attritions_attrition_id on attritions(attrition_id);
+
+Create index idx_credit_clients_client_number on credit_clients(client_number);
+
+Create index idx_education_levels_education_id on education_levels(education_id);
+
+Create index idx_incomes_income_id on incomes(income_id);
+
+Create index idx_marital_statuses_mar_status_id on marital_statuses(mar_status_id);
+
+
